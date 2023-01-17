604f90d4b0b3d700ab4cb3bc0cde13ebfacce448
Shubhanshu Gupta <91589008+Gptshubh@users.noreply.github.com>
2023-01-17 22:57:00 +0530

    Add files via upload

--- OTP.py
+++ OTP.py
@@ -1,4 +1,4 @@
-import os
+from flask import Flask, render_template, request
 import math
 import random
 import smtplib
@@ -11,11 +11,44 @@ otp = OTP + " is your OTP"
 msg= otp
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
-s.login("subhanshugupta1995@gmail.com", "jcrgkieqvpqfurla")
-emailid = input("Enter your email: ")
-s.sendmail('&&&&&&&&&&&',emailid,msg)
-a = input("Enter Your OTP >>: ")
-if a == OTP:
-    print("Verified")
-else:
-    print("Please Check your OTP again")
+s.login("subhanshugupta1995@gmail.com", "zgqvkedzzdcvpbjc")
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/generate",methods=['post'])
+def generate():
+
+    email=""
+    email=request.form.get('email')
+    s.sendmail('&&&&&&&&&&&',email,msg)
+
+    return render_template('verify.html')
+
+@app.route('/verify',methods=['post'])
+def verify():
+    res=""
+    user_otp=""
+
+    user_otp+=request.form.get('ist')
+    user_otp+=request.form.get('sec')
+    user_otp+=request.form.get('third')
+    user_otp+=request.form.get('fourth')
+    user_otp+=request.form.get('fifth')
+    user_otp+=request.form.get('sixth')
+
+    if user_otp==OTP:
+        res="Verified"
+    else:
+        res="Not Verified"
+    return render_template('result.html',res=res)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
